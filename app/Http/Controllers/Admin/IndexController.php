426ee0fd179d4c71642245348f173a12aedb33b3
helloworld2015cx <18289768853@163.com>
2016-07-01 15:29:33 +0800

    init create project

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/1
+ * Time: 12:47
+ */
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+
+    public function index(){
+        dump('Hello World !');
+    }
+
+
+
+
+}
