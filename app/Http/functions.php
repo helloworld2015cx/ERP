33bfa2998eaa6fd3d73cf6b9ef1d5f2a24eb9232
cheng <18289768853@163.com>
2016-07-04 22:53:10 +0800

    assets() function modify

--- app/Http/functions.php
+++ app/Http/functions.php
@@ -9,6 +9,12 @@ if(!function_exists('assets')){
     function assets($path_to_file){
 //        $server_scheme = $_SERVER['REQUEST_SCHEME'];
 //        $server_host = $_SERVER['HTTP_HOST'];
+        $enterFile =explode('/' , trim($_SERVER['SCRIPT_NAME'] , '/'));
+        if(sizeof($enterFile) > 1){
+            array_pop($enterFile);
+            $enterPath = implode('/' , $enterFile);
+            return '/'.$enterPath.'/assets/'.$path_to_file;
+        }
         return '/assets/'.$path_to_file;
     }
 }
