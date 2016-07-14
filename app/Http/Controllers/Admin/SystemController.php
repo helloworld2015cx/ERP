4a24fc37545a8ebe6dff7b7e0006c96fa19993fb
cheng <18289768853@163.com>
2016-07-14 22:39:37 +0800

    merge files

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -9,17 +9,36 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
-//use Model\Users\User;
+use Model\Users\User;
 
 
 class SystemController extends Controller
 {
 
     public function index(){
-//        $current_user = User::getUserIdentity(session('user_id'));
-//        dump(session());
-//        dump($current_user);
-        dump(Cache::get('current_user'));
+        $current_user = User::getUserIdentity(session('user_id'));
+        dump($current_user['menus']);
+//        $data = [[1,'hello','world'],[0,'cheng','xiang'],[2,'shao','kai']];
+//        $order = [1,0,2];
+//        array_multisort($order , SORT_ASC , $data);
+//        dump($data);
+//        dump(Cache::get('current_user'));
+
+//        $testArr = [0,1,2,[2,3,0,['Hello','world']],['cheng','xiang']];
+//        self::recursivePrint($testArr);
+
+    }
+
+    private static function recursivePrint($testArr)
+    {
+        foreach($testArr as $key=>$value) {
+            if(is_array($value)) {
+                self::recursivePrint($value);
+            } else {
+                echo $value."<br>";
+            }
+        }
+
     }
 
 }
